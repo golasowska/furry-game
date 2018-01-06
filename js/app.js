083acf342a0a92b29a8293d4a0fa8444598915ac
Anna Golasowska <annagolasowska@gmail.com>
2018-01-06 19:36:35 +0100

    added new methods: gameOver and checkCollision

--- js/app.js
+++ js/app.js
@@ -42,14 +42,22 @@ document.addEventListener('DOMContentLoaded', function(){
     }
     moveFurry(){
       if(this.furry.direction === 'right') {
-          this.furry.x = this.furry.x + 1;
+          this.furry.x++;
       } else if (this.furry.direction === 'left'){
-        this.furry.x = this.furry.x -1;
+        this.furry.x--;
       } else if (this.furry.direction === 'up') {
-        this.furry.y = this.furry.y -1;
+        this.furry.y--;
       } else {
-        this.furry.y = this.furry.y +1;
-      }this.showFurry();
+        this.furry.y++;
+      }
+      if (this.furry.x < 0 || this.furry.x  > 9 || this.furry.y < 0 || this.furry.y > 9){
+        this.gameOver();
+      } else {
+        this.showFurry();
+        this.checkCoinCollision();
+      }
+
+
     }
     hideVisibleFurry(){
       let visFurry = document.querySelector('.furry');
@@ -74,6 +82,22 @@ document.addEventListener('DOMContentLoaded', function(){
           break;
         }
     }
+
+    checkCoinCollision(){
+      if (this.furry.x === this.coin.x && this.furry.y === this.coin.y) {
+        this.board[this.index(this.coin.x, this.coin.y)].classList.remove('coin');
+        this.score++;
+        document.querySelector('#score div strong').innerText = this.score;
+        this.coin = new Coin();
+        this.showCoin();
+      }
+    }
+
+    gameOver(){
+        clearInterval(this.idSetInterval);
+        this.hideVisibleFurry();
+    }
+
   };
 
   document.addEventListener('keydown', function(event){
